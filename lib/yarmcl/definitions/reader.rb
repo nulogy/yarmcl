fb3a14cfc395d976990009015ee5f3371639e52b
Evan Brodie <brodie.evan@gmail.com>
2018-07-06 15:52:29 -0400

    Removes Rails and uses modern YAML module for the Reader class

--- lib/yarmcl/definitions/reader.rb
+++ lib/yarmcl/definitions/reader.rb
@@ -2,10 +2,9 @@ module YARMCL
   module Definitions
     class Reader
       def read(*path_elements)
-        # TODO: Define a better way to input a particular file path
-        definitions_file = Rails.root.join("example", *path_elements)
+        definitions_file = File.join(*path_elements)
 
-        YAML.load_file(definitions_file)
+        Psych.load_file(definitions_file)
       end
     end
   end
