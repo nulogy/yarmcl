445dbb3cb3eee2734271d771bad2e7aa100e0119
Evan Brodie <brodie.evan@gmail.com>
2018-07-06 18:01:50 -0400

    Adds generator_library to constructor

--- lib/yarmcl/generators/catalog_generator.rb
+++ lib/yarmcl/generators/catalog_generator.rb
@@ -2,11 +2,17 @@ module YARMCL
   module Generators
     class CatalogGenerator
       # This will be the entry point for catalog generation
-      def initialize(*) end
+      def initialize(generator_library)
+        @generator_library = generator_library
+      end
 
       def generate_catalog
-        nil
+        
       end
+
+      private
+
+      attr_reader :generator_library
     end
   end
 end
