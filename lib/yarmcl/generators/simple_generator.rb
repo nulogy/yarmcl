900d00338f3d7981fd90a2936757f477fce3f4d4
Evan Brodie <brodie.evan@gmail.com>
2018-07-09 00:24:15 -0400

    Follows a new YAML formatting standard

--- lib/yarmcl/generators/simple_generator.rb
+++ lib/yarmcl/generators/simple_generator.rb
@@ -5,9 +5,9 @@ module YARMCL
     class SimpleGenerator < GeneratorBase
       def generate
         OpenStruct.new.tap do |instances|
-          instance_definitions.each do |instance_definition|
-            tag = instance_definition.delete("tag")
-            instances.send("#{tag}=", generate_instance(instance_definition))
+          instance_data = instance_definitions.fetch("data")
+          instance_data.each_pair do |key, value|
+            instances.send("#{key}=", generate_instance(value))
           end
         end
       end
